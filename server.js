d876b33675589df03c6e321a2660f9f476b8dd97
Hopkins <robin.hopkins@cgi.com>
2018-04-07 16:05:42 +0100

    add mongo console logs

--- server.js
+++ server.js
@@ -12,7 +12,9 @@ var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
     ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0',
     mongoURL = process.env.OPENSHIFT_MONGODB_DB_URL || process.env.MONGO_URL,
     mongoURLLabel = "";
-
+    
+console.log('starting up');    
+    
 if (mongoURL == null && process.env.DATABASE_SERVICE_NAME) {
   var mongoServiceName = process.env.DATABASE_SERVICE_NAME.toUpperCase(),
       mongoHost = process.env[mongoServiceName + '_SERVICE_HOST'],
@@ -36,13 +38,18 @@ var db = null,
     dbDetails = new Object();
 
 var initDb = function(callback) {
-  if (mongoURL == null) return;
+  if (mongoURL == null) {
+    console.log('mongoURL null'); 
+    return;
+  }
+  console.log('connecting to mongo ' + mongoURL); 
 
   var mongodb = require('mongodb');
   if (mongodb == null) return;
 
   mongodb.connect(mongoURL, function(err, conn) {
     if (err) {
+      console.log('error connecting to mongo ' + err); 
       callback(err);
       return;
     }
