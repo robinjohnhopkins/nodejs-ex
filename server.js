07765cbe43ce89140c35158f52117a8abcb7a356
Robin John Hopkins <robinjohnhopkins@Robins-MacBook-Pro.local>
2018-04-29 13:30:19 +0100

    server.js re-remove html suffix

--- server.js
+++ server.js
@@ -90,10 +90,10 @@ app.get('/', function (req, res) {
       if (err) {
         console.log('Error running count. Message:\n'+err);
       }
-      res.render('index.html', { pageCountMessage : count, dbInfo: dbDetails });
+      res.render('index', { pageCountMessage : count, dbInfo: dbDetails });
     });
   } else {
-    res.render('index.html', { pageCountMessage : null});
+    res.render('index', { pageCountMessage : null});
   }
 });
 
