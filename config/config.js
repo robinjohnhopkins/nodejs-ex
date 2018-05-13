0541d5b42ec3757049eabb8d7d55d98464cb4227
Robin John Hopkins <robinjohnhopkins@Robins-MacBook-Pro.local>
2018-05-13 15:02:25 +0100

    added some extra pages with css experiment

---
+++ config/config.js
@@ -0,0 +1,36 @@
+var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
+    ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0',
+    mongoURL = process.env.OPENSHIFT_MONGODB_DB_URL || process.env.MONGO_URL,
+    mongoURLLabel = "";
+
+if (mongoURL == null && process.env.DATABASE_SERVICE_NAME) {
+  var mongoServiceName = process.env.DATABASE_SERVICE_NAME.toUpperCase(),
+      mongoHost = process.env[mongoServiceName + '_SERVICE_HOST'],
+      mongoPort = process.env[mongoServiceName + '_SERVICE_PORT'],
+      mongoDatabase = process.env[mongoServiceName + '_DATABASE'],
+      mongoPassword = process.env[mongoServiceName + '_PASSWORD']
+      mongoUser = process.env[mongoServiceName + '_USER'];
+      console.log(mongoServiceName, mongoHost, mongoPort, mongoDatabase, 
+        mongoPassword, mongoUser );
+
+  if (mongoHost && mongoPort && mongoDatabase) {
+    mongoURLLabel = mongoURL = 'mongodb://';
+    if (mongoUser && mongoPassword) {
+      mongoURL += mongoUser + ':' + mongoPassword + '@';
+    }
+    // Provide UI label that excludes user id and pw
+    mongoURLLabel += mongoHost + ':' + mongoPort + '/' + mongoDatabase;
+    mongoURL += mongoHost + ':' +  mongoPort + '/' + mongoDatabase;
+
+  }
+}
+if (mongoURL == null){
+  // mongodb://[username:password@]host1[:port1][,host2[:port2],...[,hostN[:portN]]][/[database][?options]]
+  mongoURL='mongodb://mongo:mongo@localhost:27017/exampledb?authSource=admin';
+}
+module.exports = {
+        port: port,
+        ip: ip,
+        mongoURLLabel : mongoURLLabel,
+        mongoURL:mongoURL
+    };
--- server.js
+++ server.js
@@ -1,20 +1,21 @@
 //  OpenShift sample Node application
 var express = require('express'),
-    app     = express(),
-    morgan  = require('morgan');
+  app = express(),
+  morgan = require('morgan');
 const path = require('path')
 
-Object.assign=require('object-assign')
+Object.assign = require('object-assign')
 var passport = require('passport');
 var Strategy = require('passport-facebook').Strategy;
+var config = require('./config/config'); //load the config
 
 var APP_VERSION = '1.0.0-1';
-var clientId = process.env.FACEBOOK_ID;
-var clientSecret = process.env.FACEBOOK_SECRET;
-console.log('clientId=' + clientId + ' clientSecret=' + clientSecret);    
+var clientId = process.env.FACEBOOK_ID || 'fakeid';
+var clientSecret = process.env.FACEBOOK_SECRET || 'fakesecret';
+console.log('clientId=' + clientId + ' clientSecret=' + clientSecret);
 //var clientId = '1492519200858678';
 //var clientSecret = '4904b011ab92bb1c4340f78b3a33aee7';
-var callback='https://nodejs-mongo-persistent-rjhnode.193b.starter-ca-central-1.openshiftapps.com/login/facebook/return';
+var callback = 'https://nodejs-mongo-persistent-rjhnode.193b.starter-ca-central-1.openshiftapps.com/login/facebook/return';
 
 // Configure the Facebook strategy for use by Passport.
 //
@@ -28,7 +29,7 @@ passport.use(new Strategy({
     clientSecret: clientSecret,
     callbackURL: callback
   },
-  function(accessToken, refreshToken, profile, cb) {
+  function (accessToken, refreshToken, profile, cb) {
     // In this example, the user's Facebook profile is supplied as the user
     // record.  In a production-quality application, the Facebook profile should
     // be associated with a user record in the application's database, which
@@ -47,89 +48,75 @@ passport.use(new Strategy({
 // from the database when deserializing.  However, due to the fact that this
 // example does not have a database, the complete Facebook profile is serialized
 // and deserialized.
-passport.serializeUser(function(user, cb) {
+passport.serializeUser(function (user, cb) {
   cb(null, user);
 });
 
-passport.deserializeUser(function(obj, cb) {
+passport.deserializeUser(function (obj, cb) {
   cb(null, obj);
 });
 
 app.engine('html', require('ejs').renderFile);
 app.use(morgan('combined'))
+app.use('/', express.static(__dirname + '/node_modules/bootstrap/dist/'));
+app.use('/js', express.static(__dirname + '/node_modules/jquery/dist'));
+app.use('/js', express.static(__dirname + '/node_modules/popper.js/dist'));
+app.use('/', express.static(__dirname + '/node_modules/feather-icons/dist'));
+app.use('/', express.static(__dirname + '/node_modules/chart.js/dist'));
+
+app.use('/', express.static(__dirname + '/public'));
 
 app.set('views', path.join(__dirname, 'views'))
-    .set('view engine', 'ejs')
+  .set('view engine', 'ejs')
 
 
-console.log('process.env');    
-console.log(process.env);    
+console.log('process.env');
+console.log(process.env);
+console.log('starting up, config is');
+console.log(config);
 
-var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
-    ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0',
-    mongoURL = process.env.OPENSHIFT_MONGODB_DB_URL || process.env.MONGO_URL,
-    mongoURLLabel = "";
-    
-console.log('starting up');    
-console.log(mongoURL);
-
-
-if (mongoURL == null && process.env.DATABASE_SERVICE_NAME) {
-  var mongoServiceName = process.env.DATABASE_SERVICE_NAME.toUpperCase(),
-      mongoHost = process.env[mongoServiceName + '_SERVICE_HOST'],
-      mongoPort = process.env[mongoServiceName + '_SERVICE_PORT'],
-      mongoDatabase = process.env[mongoServiceName + '_DATABASE'],
-      mongoPassword = process.env[mongoServiceName + '_PASSWORD']
-      mongoUser = process.env[mongoServiceName + '_USER'];
-      console.log(mongoServiceName, mongoHost, mongoPort, mongoDatabase, 
-        mongoPassword, mongoUser );
-
-  if (mongoHost && mongoPort && mongoDatabase) {
-    mongoURLLabel = mongoURL = 'mongodb://';
-    if (mongoUser && mongoPassword) {
-      mongoURL += mongoUser + ':' + mongoPassword + '@';
-    }
-    // Provide UI label that excludes user id and pw
-    mongoURLLabel += mongoHost + ':' + mongoPort + '/' + mongoDatabase;
-    mongoURL += mongoHost + ':' +  mongoPort + '/' + mongoDatabase;
-
-  }
-}
 var db = null,
-    dbDetails = new Object();
+  dbDetails = new Object();
 
-var initDb = function(callback) {
-  if (mongoURL == null) {
-    console.log('mongoURL null'); 
+var initDb = function (callback) {
+  if (config.mongoURL == null) {
+    console.log('mongoURL null');
     return;
     //mongoURL = "mongodb://10.128.36.106:"
   }
-  console.log('connecting to mongo ' + mongoURL); 
+
+  console.log('connecting to mongo ' + config.mongoURL);
 
   var mongodb = require('mongodb');
   if (mongodb == null) return;
 
-  mongodb.connect(mongoURL, function(err, conn) {
+  mongodb.connect(config.mongoURL, function (err, conn) {
     if (err) {
-      console.log('error connecting to mongo ' + err); 
+      console.log('error connecting to mongo ' + err);
       callback(err);
       return;
     }
 
     db = conn;
     dbDetails.databaseName = db.databaseName;
-    dbDetails.url = mongoURLLabel;
+    dbDetails.url = config.mongoURLLabel;
     dbDetails.type = 'MongoDB';
 
-    console.log('Connected to MongoDB at: %s', mongoURL);
+    console.log('Connected to MongoDB at: %s', config.mongoURL);
   });
 };
 
 // Use application-level middleware for common functionality, including
 // logging, parsing, and session handling.
 app.use(require('cookie-parser')());
-app.use(require('body-parser').urlencoded({ extended: true }));
-app.use(require('express-session')({ secret: 'keyboard cat', resave: true, saveUninitialized: true }));
+app.use(require('body-parser').urlencoded({
+  extended: true
+}));
+app.use(require('express-session')({
+  secret: 'keyboard cat',
+  resave: true,
+  saveUninitialized: true
+}));
 
 // Initialize Passport and restore authentication state, if any, from the
 // session.
@@ -140,20 +127,33 @@ app.get('/', function (req, res) {
   // try to initialize the db on every request if it's not already
   // initialized.
   if (!db) {
-    initDb(function(err){});
+    initDb(function (err) {});
   }
   if (db) {
     var col = db.collection('counts');
     // Create a document with request IP and current time of request
-    col.insert({ip: req.ip, date: Date.now()});
-    col.count(function(err, count){
+    col.insert({
+      ip: req.ip,
+      date: Date.now()
+    });
+    col.count(function (err, count) {
       if (err) {
-        console.log('Error running count. Message:\n'+err);
+        console.log('Error running count. Message:\n' + err);
       }
-      res.render('index', { pageCountMessage : count, dbInfo: dbDetails, user: req.user, appVersion : APP_VERSION });
+      res.render('index', {
+        pageCountMessage: count,
+        dbInfo: dbDetails,
+        user: req.user,
+        appVersion: APP_VERSION
+      });
     });
   } else {
-    res.render('index', { pageCountMessage : null, user: req.user });
+    res.render('index', {
+      pageCountMessage: null,
+      dbInfo: 'na',
+      user: req.user,
+      appVersion: APP_VERSION
+    });
   }
 });
 
@@ -165,36 +165,56 @@ app.get('/login', function (req, res) {
     res.send("Error login " + err);
   }
 });
+app.get('/map', function (req, res) {
+  try {
+    res.render('map');
+  } catch (err) {
+    console.error(err);
+    res.send("Error map " + err);
+  }
+});
+app.get('/dashboard', function (req, res) {
+  try {
+    res.render('dashboard');
+  } catch (err) {
+    console.error(err);
+    res.send("Error dashboard " + err);
+  }
+});
 app.get('/login/facebook',
   passport.authenticate('facebook'));
 
-app.get('/login/facebook/return', 
-  passport.authenticate('facebook', { failureRedirect: '/login' }),
-  function(req, res) {
-    console.log('/login/facebook/return');    
+app.get('/login/facebook/return',
+  passport.authenticate('facebook', {
+    failureRedirect: '/login'
+  }),
+  function (req, res) {
+    console.log('/login/facebook/return');
     console.log(res);
     res.redirect('/');
   });
-  
-app.get('/logout', function(req, res) {
-    req.logout(); 
-    res.redirect('/');
+
+app.get('/logout', function (req, res) {
+  req.logout();
+  res.redirect('/');
 });
 
 app.get('/profile',
   require('connect-ensure-login').ensureLoggedIn(),
-  function(req, res){
-    res.render('profile', { user: req.user });
+  function (req, res) {
+    res.render('profile', {
+      user: req.user
+    });
   });
 
 app.get('/pagecount', function (req, res) {
   // try to initialize the db on every request if it's not already
   // initialized.
   if (!db) {
-    initDb(function(err){});
+    initDb(function (err) {});
   }
   if (db) {
-    db.collection('counts').count(function(err, count ){
+    db.collection('counts').count(function (err, count) {
       res.send('{ pageCount: ' + count + '}');
     });
   } else {
@@ -203,16 +223,16 @@ app.get('/pagecount', function (req, res) {
 });
 
 // error handling
-app.use(function(err, req, res, next){
+app.use(function (err, req, res, next) {
   console.error(err.stack);
   res.status(500).send('Something bad happened!');
 });
 
-initDb(function(err){
-  console.log('Error connecting to Mongo. Message:\n'+err);
+initDb(function (err) {
+  console.log('Error connecting to Mongo. Message:\n' + err);
 });
 
-app.listen(port, ip);
-console.log('Server running on http://%s:%s', ip, port);
+app.listen(config.port, config.ip);
+console.log('Server running on http://%s:%s', config.ip, config.port);
 
-module.exports = app ;
+module.exports = app;
